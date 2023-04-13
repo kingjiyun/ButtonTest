bc54c7fcdfab0993209ab63b4fc8e20a4fae9c67
Kingjiyun <etock19@naver.com>
2023-04-13 20:04:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/buttontest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.buttontest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        var btn1: Button
+//        btn1 = findViewById<Button>(R.id.btn1)
+        setContentView(R.layout.layout3)
+    }
+}
